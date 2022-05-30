733b71dc45eb8d2143dbadf495909d0a61ae934b
Prasit Champathong <prasit.c@cjworx.com>
2022-05-30 14:57:27 +0700

    fetch api

--- pages/[pid].js
+++ pages/[pid].js
@@ -1,15 +1,18 @@
-import { useRouter } from 'next/router'
-const Post = () => {
-    const router = useRouter()
-    const { pid } = router.query
-    const url = `http://www.google.com/${pid}`
-  
+function Page({ data }) {
     return (
-      <>
-        <h1>Post: {pid}</h1>
-        <p>{url}</p>
-      </>
-    )
-  }
-  
-  export default Post
+        <>
+            <h1>{data.title}</h1>
+            <p>{data.id}</p>
+        </>
+    );
+}
+export async function getServerSideProps({params}) {
+    const pid = params.pid
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${pid}`)
+    const data = await res.json()
+
+    return {
+      props: { data },
+    }
+}
+export default Page
