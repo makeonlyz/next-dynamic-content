44ad2db4a7c38cee72f6fda2910ab98cae75ee02
makeonlyz <steamkeyshop@gmail.com>
2022-05-30 18:53:09 +0700

    Update [pid].js

--- pages/[pid].js
+++ pages/[pid].js
@@ -1,14 +1,23 @@
+import { useRouter } from 'next/router'
+import axios from 'axios';
+
+//97409
+
 function Page({ data }) {
     return (
-        <>
-            <h1>{data.title}</h1>
-            <p>{data.id}</p>
-        </>
+       <> 
+            <h1>{data.id}</h1>
+            <p>{data.title['rendered']}</p>
+            <p>{data.content['rendered']}</p>
+       </>
     );
 }
+
+
+
 export async function getServerSideProps({params}) {
     const pid = params.pid
-    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${pid}`)
+    const res = await fetch(`https://lolitopia.com/?rest_route=/wp/v2/posts/${pid}`)
     const data = await res.json()
 
     return {
@@ -16,3 +25,5 @@ export async function getServerSideProps({params}) {
     }
 }
 export default Page
+
+
